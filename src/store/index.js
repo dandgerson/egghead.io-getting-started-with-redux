88c8959bece40357b3acc3710f39f9986095dfc3
Dmitry G. Anderson <dandgerson@gmail.com>
2020-08-09 14:40:49 +0300

    12 wrapping dispatch with log actions

--- src/store/index.js
+++ src/store/index.js
@@ -23,6 +23,28 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch
+
+  if (!console.group) return rawDispatch
+
+  return (action) => {
+    console.group(action.type)
+    console.log('%c prev state', 'color: grey', store.getState())
+    console.log('%c action', 'color: blue', action)
+    const returnValue = rawDispatch(action)
+    console.log('%c next state', 'color: green', store.getState())
+    console.groupEnd(action.type)
+
+    return returnValue
+  }
+}
+
+
+if (process.env.NODE_ENV !== 'production') {
+  store.dispatch = addLoggingToDispatch(store)
+}
+
 store.subscribe(throttle(() => {
   const { todos } = store.getState()
 
